d99ce9afc9bf5f2b997a54ef8eb2d7585bc1c004
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-03-23 22:26:08 +0300

    initial commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: [
+    '../app.component.css',
+    './about.component.css'
+  ]
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
